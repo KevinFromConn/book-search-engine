56757f6b1fc29f8c03589e9bc545b2d57a46b280
KevinFromConn <kfc21194@gmail.com>
2021-10-17 21:04:31 -0400

    made necessary changes to utils/auth.js and server.js, added schemas folder with necessary files

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+      _id: ID
+      username: String!
+      email: String!
+      password: String!
+      savedBooks: [Book]
+      bookCount: String!
+  }
+  
+  type Book {
+      authors: []
+      description: String!
+      bookId: Int!
+      title: String!
+      image: String
+      link: String
+  }
+
+  type Query {
+      me: User
+  }
+
+  input SaveBookInput {
+      bookId: String!
+      authors: [String]
+      description: String!
+      title: String!
+      image: String
+      link: String
+  }
+
+  type Mutation {
+      login(email: String!, password: String!): Auth
+      addUser(username: String!, email: String!, password: String!): Auth
+      saveBook(input: SaveBookInput): User
+      removeBook(bookId: String!): User
+  }
+
+  type Auth {
+      token: ID!
+      user: User
+  }
+`;
+
+module.exports = typeDefs;
